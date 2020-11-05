5c8c28507d4058e09910dd95d4bceafd307a4409
Andrei <andreyka_ilin@mail.ru>
2020-11-05 12:58:15 +0300

    LocksWait script has been changed

--- Bash/LocksWait.sh
+++ Bash/LocksWait.sh
@@ -1,7 +1,11 @@
-cat Locks/rphost*/*.log | sed -r 's/\xef\xbb\xbf//g' |
+cat Locks/rphost*/*.log |
+sed -r 's/\xef\xbb\xbf//g' |
 gawk -vORS='<LineBreak>' '{if (match($0, "^[0-9]+:[0-9]+\\.")) print "<LineBreak>\n"$0; else print $0}' |
 egrep ',TLOCK,.*WaitConnections=[0-9]+.*Context=' |
-sed -r 's/^[0-9]+:[0-9]+\.[0-9]+-//;s/,TLOCK,.*Regions=/<Regions>(/;s/,.*Context=/) Context=/' |
-gawk -F '<Regions>' '{Dur[$2]+=$1; Count[$2]+=1; Total+=$1}END{for (i in Dur) {print Dur[i]/1000000 " (сек.) Длительность; " Dur[i]/1000000/Count[i] " Средняя длит-ость (сек.); " Count[i] " Кол-во; " Dur[i]/Total*100 "% вклад;<LineBreak>--------------------------------------------------------------------------------------------<LineBreak>" i}}' |
+sed -r 's/^[0-9]+:[0-9]+\.[0-9]+-//' | 
+sed -r 's/,TLOCK,.*p:processName=/|Base=/' |
+sed -r 's/,.*Regions='\''?/, Regions=/' |
+sed -r 's/'\''?,Locks=.*Context=/, Context=/' |
+gawk -F '|' '{Dur[$2]+=$1; Count[$2]+=1; Total+=$1}END{for (i in Dur) {print Dur[i]/1000000 " (сек.) Длительность; " Dur[i]/1000000/Count[i] " Средняя длит-ость (сек.); " Count[i] " Кол-во; " Dur[i]/Total*100 "% вклад;<LineBreak>--------------------------------------------------------------------------------------------<LineBreak>" i}}' |
 sort -rnb |
 sed -r 's/<LineBreak>/\n/g' > LocksWait.txt
